c6dc9d84b92c2209cf50806f6d3183f84c2a9081
LC1B_TakahashiKouta <k021g1164@g.neec.ac.jp>
2022-05-25 11:46:36 +0900

    かり

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -1,9 +1,8 @@
-#include "Basic.hlsli"
-
-Texture2D<float4>tex : register(t0);
-SamplerState smp : register(s0);
-
-float4 main(VSOutput input) : SV_TARGET
+﻿cbuffer ConstBufferDataMaterial:register(b0)
 {
-	return float4(tex.Sample(smp,input.uv));
+	float4 color; //F(RGBA)
+}
+float4 main() : SV_TARGET
+{
+	return color;
 }
--- BasicVS.hlsl
+++ BasicVS.hlsl
@@ -1,14 +1,4 @@
-#include "Basic.hlsli"
-
-
 float4 main(float4 pos : POSITION) : SV_POSITION
 {
 	return pos;
-}
-VSOutput main(float4 pos : POSITION, float2 uv : TEXCOORD)
-{
-	VSOutput output;
-	output.svpos = pos;
-	output.uv = uv;
-	return output;
 }
