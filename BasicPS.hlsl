3edfe03f0ea852e30296c1bc66903c98c632c2e7
LC1B_TakahashiKouta <k021g1164@g.neec.ac.jp>
2022-07-11 10:17:26 +0900

    深度できない

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -1,11 +1,15 @@
 #include "Basic.hlsli"
+#pragma once
 
 Texture2D<float4>tex : register(t0);
 SamplerState smp : register(s0);
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	return float4(input.normal,1);//RGB‚ğ‚»‚ê‚¼‚ê–@ü‚ÌXYZAA‚ğ1‚Åo—Í
-    //return float4(tex.Sample(smp,input.uv)) * color;
-	//return float4(1,1,1,1);
+	float4 texcolor = float4(tex.Sample(smp,input.uv));
+	float3 light = normalize(float3(1,-1,1));
+	float diffuse = saturate(dot(-light, input.normal));
+	float brightness = diffuse + 0.3f;
+	return float4(texcolor.rgb * brightness, texcolor.a) * color;
+
 }
