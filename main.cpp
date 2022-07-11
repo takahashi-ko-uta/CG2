a4604fc1453618e9c54d817cb19c156288c91356
LC1B_TakahashiKouta <k021g1164@g.neec.ac.jp>
2022-07-11 11:12:11 +0900

    深度できない

--- main.cpp
+++ main.cpp
@@ -717,10 +717,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		XMMATRIX mat; //3D変換行列
 	};
 
-	ID3D12Resource* constBuffTransform = nullptr;
-
-	ConstBufferDataTransform* constMapTransform = nullptr;
-
+#pragma region Material
 	//ヒープ設定
 	ID3D12Resource* constBuffMaterial = nullptr;
 	ConstBufferDataMaterial* constMapMaterial = nullptr;
@@ -754,10 +751,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		result = constBuffMaterial->Map(0, nullptr, (void**)&constMapMaterial);//マッピング
 		assert(SUCCEEDED(result));
-
-		ID3D12Resource* constBuffTransform = nullptr;
-		ConstBufferDataTransform* constMapTransform = nullptr;
 	}
+#pragma endregion
+
+#pragma region Transform
+
+	ID3D12Resource* constBuffTransform0 = nullptr;
+	ConstBufferDataTransform* constMapTransform0 = nullptr;
 	{
 		//ヒープ設定
 		D3D12_HEAP_PROPERTIES cbHeapProp{};
@@ -780,17 +780,53 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			&cbResourceDesc,//リソース設定
 			D3D12_RESOURCE_STATE_GENERIC_READ,
 			nullptr,
-			IID_PPV_ARGS(&constBuffTransform));
+			IID_PPV_ARGS(&constBuffTransform0));
 		assert(SUCCEEDED(result));
 
 		//定数バッファのマッピング
-		result = constBuffTransform->Map(0, nullptr, (void**)&constMapTransform);//マッピング
+		result = constBuffTransform0->Map(0, nullptr, (void**)&constMapTransform0);//マッピング
 		assert(SUCCEEDED(result));
 	}
 
+	ID3D12Resource* constBuffTransform1 = nullptr;
+	ConstBufferDataTransform* constMapTransform1 = nullptr;
+	{
+		//ヒープ設定
+		D3D12_HEAP_PROPERTIES cbHeapProp{};
+		cbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD;//GPUへの転送用
+
+		//リソース設定
+		D3D12_RESOURCE_DESC cbResourceDesc{};
+		cbResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+		cbResourceDesc.Width = (sizeof(ConstBufferDataTransform) + 0xff) & ~0xff;
+		cbResourceDesc.Height = 1;
+		cbResourceDesc.DepthOrArraySize = 1;
+		cbResourceDesc.MipLevels = 1;
+		cbResourceDesc.SampleDesc.Count = 1;
+		cbResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+		//定数バッファの生成
+		result = device->CreateCommittedResource(
+			&cbHeapProp,//ヒープ設定
+			D3D12_HEAP_FLAG_NONE,
+			&cbResourceDesc,//リソース設定
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr,
+			IID_PPV_ARGS(&constBuffTransform1));
+		assert(SUCCEEDED(result));
+
+		//定数バッファのマッピング
+		result = constBuffTransform1->Map(0, nullptr, (void**)&constMapTransform1);//マッピング
+		assert(SUCCEEDED(result));
+	}
+
+#pragma endregion
+
+	
 
 	//単位行列を代入
-	constMapTransform->mat = XMMatrixIdentity();
+	constMapTransform0->mat = XMMatrixIdentity();
+	constMapTransform1->mat = XMMatrixIdentity();
 
 
 	//値を書き込むと自動的に転送される
@@ -844,6 +880,29 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	scale = { 1.0f,1.0f,1.0f };
 	rotation = { 0.0f,0.0f,0.0f };
 	position = { 0.0f,0.0f,0.0f };
+
+
+	//ワールド変換行列
+	XMMATRIX matWorld1;
+
+	XMMATRIX matScala1;
+
+	XMMATRIX matRot1;
+
+	XMMATRIX matTrans1;
+
+	//スケーリング倍率
+	XMFLOAT3 scale1;
+	//回転角
+	XMFLOAT3 rotation1;
+
+	//座標
+	XMFLOAT3 position1;
+
+	scale1 = { 1.0f,1.0f,1.0f };
+	rotation1 = { 0.0f,PI / 4.0f,0.0f };
+	position1= { -20.0f,0.0f,0.0f };
+
 
 #pragma endregion
 
@@ -1083,8 +1142,25 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		matWorld *= matRot;
 		matWorld *= matTrans;
 
-		constMapTransform->mat = matWorld * matView * matProjection;
+		constMapTransform0->mat = matWorld * matView * matProjection;
 
+
+		matScala1 = XMMatrixScaling(scale1.x, scale1.y, scale1.z);
+
+		matRot1 = XMMatrixIdentity();
+		matRot1 = XMMatrixRotationZ(rotation1.z);
+		matRot1 = XMMatrixRotationX(rotation1.x);
+		matRot1 = XMMatrixRotationY(rotation1.y);
+
+		//XMMATRIX matTrans;
+		matTrans1 = XMMatrixTranslation(position1.x, position1.y, position1.z);
+
+		matWorld1 = XMMatrixIdentity();
+		matWorld1 *= matScala1;
+		matWorld1 *= matRot1;
+		matWorld1 *= matTrans1;
+
+		constMapTransform1->mat = matWorld1 * matView * matProjection;
 #pragma endregion
 		
 		
@@ -1185,12 +1261,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		//SRVヒープの先頭にあるSRVをルートパラメータ1番に設定
 		commandList->SetGraphicsRootDescriptorTable(1, srvGpuHandle);
 		//定数バッファビュー(SRV)の設定コマンド
-		commandList->SetGraphicsRootConstantBufferView(2, constBuffTransform->GetGPUVirtualAddress());
-#pragma endregion
-
-#pragma region 描画コマンド
-		//描画コマンド
+		commandList->SetGraphicsRootConstantBufferView(2, constBuffTransform0->GetGPUVirtualAddress());
+		// 描画コマンド
 		commandList->DrawIndexedInstanced(_countof(indices), 1, 0, 0, 0);//全ての頂点を使って描画
+		//定数バッファビュー(SRV)の設定コマンド
+		commandList->SetGraphicsRootConstantBufferView(2, constBuffTransform1->GetGPUVirtualAddress());
+		// 描画コマンド
+		commandList->DrawIndexedInstanced(_countof(indices), 1, 0, 0, 0);//全ての頂点を使って描画
+
 #pragma endregion
 	
 #pragma endregion
